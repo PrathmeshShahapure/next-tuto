f49d8b7ee367da244ba845e8bfa88a8cf528a134
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-07-15 14:54:35 +0530

    Ex 9 dynamic Intercepting routes

--- app/@modal/(.)photo/[id]/page.tsx
+++ app/@modal/(.)photo/[id]/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import { images } from "@/data/data";
+const Shortview = async({ params }: { params: Promise<{id:string} >}) => {
 
-const Shortview = () => {
+  const { id } = await params;
+
+  const IMG = images.find((i) => +id === i.id);
   return (
     <div className="  fixed inset-0 bg-black/50 min-h-screen  flex justify-center items-center">
       <Image
@@ -11,7 +15,7 @@ const Shortview = () => {
         className="z-10 bg-white p-5 "
         width={400}
         height={600}
-        src="https://images.unsplash.com/photo-1768970338809-d88ab5997672?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={ IMG?.url}
       ></Image>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
+import { images } from "@/data/data";
 export default function Home() {
-  return (
-    <div className="text-center">
-      PhotosGram
-      <Link
+  
+  return (<>
+  <h1 className="mt-5 text-center">   PhotosGram</h1>
+    <div className=" grid grid-cols-3 p-10">
+   
+      {images.map((i) => (
+        <Link
+        href={`photo/${i.id}`}
+        key={i.id}
         className="flex flex-col justify-center items-center m-20  "
-        href="/photo"
-      >
-        <Image
-          alt="my-home"
-           className="w-auto h-auto"
-          width={500}
-          height={500}
-          loading="eager"
-          src="https://images.unsplash.com/photo-1768970338809-d88ab5997672?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ></Image>
-      </Link>
+        >
+          <Image
+            alt="my-home"
+            width={200}
+            height={300}
+            loading="eager"
+            src={i.url}
+            ></Image>
+        </Link>
+      ))}
     </div>
+      </>
   );
 }
