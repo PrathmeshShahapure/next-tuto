dcb1249c41e9a6a103e122b1a31b097154f97174
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-07-17 11:11:34 +0530

    learn: Zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,48 @@
-import Image from "next/image";
-import Link from "next/link";
-import { images } from "@/data/data";
+import { z } from "zod"
+
+const userSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  age: z.number().min(16).optional(),
+  profileUrl: z.string().url(),
+  friends: z.array(z.string().min(1)),
+  settings: z.object({ isOn:z.boolean(),metalName:z.string()})
+})
+
+
+
 export default function Home() {
+ 
+  type Users = z.infer<typeof userSchema>
   
+   const user1:Users = {
+    name: "Prathmesh",
+    email: "prath@ga.co",
+    age: 20,
+    profileUrl: "http://prathlive",
+    friends: ["Prathmesh", "Pratik"],
+    settings: {isOn:false,metalName:"iron_721"}
+  }
+
+  const user2 = {
+    name: "Prathmesh",
+    email: "prath@ga.co",
+    age: 20,
+    profileUrl: "http://prathlive",
+    friends: ["Prathmesh", "Pratik"],
+    settings: {isOn:false,metalName:"iron_721"}
+  }
+  
+    const InvalidUser = {
+  name:1
+}
+console.log(userSchema.parse(user1));
+console.log(userSchema.parse(user2));
+console.log(userSchema.safeParse(InvalidUser));
+
   return (<>
-  <h1 className="mt-5 text-center">   PhotosGram</h1>
-    <div className=" grid grid-cols-3 p-10">
-   
-      {images.map((i) => (
-        <Link
-        href={`photo/${i.id}`}
-        key={i.id}
-        className="flex flex-col justify-center items-center m-20  "
-        >
-          <Image
-            alt="my-home"
-            width={200}
-            height={300}
-            loading="eager"
-            src={i.url}
-            ></Image>
-        </Link>
-      ))}
-    </div>
+  <h1 className="mt-5 text-2xl text-center">   PhotosGram</h1>
+  
       </>
   );
 }
